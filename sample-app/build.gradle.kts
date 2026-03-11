64e33e94c9feb9dee46345eab4abfb165c9989b1
faridGuliyew <ferid.quliyev.sec@gmail.com>
2026-03-11 14:09:45 +0400

    Remove sample-app dependency from jitpack - it causes issues in the build process

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(project(":replaydroid"))
-    implementation("com.github.faridGuliyew:replaydroid:100bfde73b")
 }
 
 val ktor_version = "3.4.1"
